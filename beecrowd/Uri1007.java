159b9a524a5f29786da62c09e6271e7ec75b8f17
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-02-03 08:39:35 -0300

    Diferença

---
+++ beecrowd/Uri1007.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]) {
+        
+        Scanner teclado = new Scanner(System.in);
+        int a, b, c, d, diferenca;
+
+        a = teclado.nextInt();
+        b = teclado.nextInt();
+        c = teclado.nextInt();
+        d = teclado.nextInt();
+
+        diferenca = (a * b - c * d);
+
+        System.out.println("DIFERENCA = " + diferenca);
+
+        teclado.close();
+    }
+}
