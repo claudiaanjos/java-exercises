74b11f977ddcc399e3682545f35346c6b496a6f4
Claudia dos Anjos <84209593+claudiaanjos@users.noreply.github.com>
2022-02-03 08:37:26 -0300

    Soma Simples

---
+++ beecrowd/Uri1003.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String args[]) {
+        
+        Scanner teclado = new Scanner(System.in);
+        int a, b , soma;
+
+        a = teclado.nextInt();
+        b = teclado.nextInt();
+
+        soma = a+b;
+
+        System.out.println("SOMA = " + soma);
+
+        teclado.close();
+    }
+}
